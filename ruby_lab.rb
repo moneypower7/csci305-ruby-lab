a698dd5c5ec303d0103d83983bf5e96340434e82
moneypower7 <toster1011@gmail.com>
2018-02-12 03:17:25 -0700

    Attempt without using String methods
    Attempt without using String methods

--- ruby_lab.rb
+++ ruby_lab.rb
@@ -10,7 +10,7 @@
 ###############################################################
 
 $bigrams = Hash.new # The Bigram data structure
-$name = "<firstname> <lastname>"
+$name = "<Troy> <Oster>"
 
 # function to process each line of a file and extract the song titles
 def process_file(file_name)
@@ -22,12 +22,14 @@ def process_file(file_name)
 			unless file.eof?
 				file.each_line do |line|
 					# do something for each line (if using windows)
+
 				end
 			end
 			file.close
 		else
 			IO.foreach(file_name, encoding: "utf-8") do |line|
 				# do something for each line (if using macos or linux)
+				puts cleanup_title(line)
 			end
 		end
 
@@ -36,6 +38,25 @@ def process_file(file_name)
 		STDERR.puts "Could not open file"
 		exit 4
 	end
+end
+#Cleans up the tile by extracting the song tile and storing it in the variable title
+def  cleanup_title(single_line)
+	if single_line =~ /[^>]*$/ #Starts at the end and finds the first > symbol and matches everything before it (the song title)
+    title = "#{$&}"
+  end
+	if title =~ /^[^(\(|\[|\{|\\|\/|\_|\-|\:|\"|\`|\+|\=|\*|)]*/ #Removes everything after these characters
+		title = "#{$&}"
+	else #this is here to prevent it form removing the songs that dont have these symbols
+	end
+	if title =~ /.*?(?=feat.)/ #Credit to user Vasilliy Ermolovich for a almost completely unrelated answer on Stack Overflow that helped me understand this
+		title = "#{$&}"
+	else
+	end
+	if title =~ /a-z[^!]/
+		title = "#{$&}"
+	else
+	end
+	return title
 end
 
 # Executes the program
